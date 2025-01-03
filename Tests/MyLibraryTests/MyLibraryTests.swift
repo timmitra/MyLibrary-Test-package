546021886b64a49d148b8b8aeb43633fb23f4fe6
Tim Mitra <tim@it-guy.com>
2025-01-03 11:48:32 -0500

    added textExample. Had to add @unchecked Sendable to build the test

--- Tests/MyLibraryTests/MyLibraryTests.swift
+++ Tests/MyLibraryTests/MyLibraryTests.swift
@@ -1,12 +1,18 @@
 import XCTest
 @testable import MyLibrary
 
-final class MyLibraryTests: XCTestCase {
+final class MyLibraryTests: XCTestCase, @unchecked Sendable {
     func testExample() throws {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
 
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+        
+        XCTAssertEqual(MyLibrary().text, "Hello, World!")
     }
+    
+    static let allTests = [
+        ("testExample", testExample),
+    ]
 }
